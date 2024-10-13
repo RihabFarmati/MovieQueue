5e3a7d0e0f321d5cf45f404f1192d70518fa8a85
rihabFarmati <rihabfarmati19@gmail.com>
2024-10-13 12:39:57 +0100

    Added MovieQueueApp

---
+++ app/src/main/java/com/store/movie/MovieQueueApp.kt
@@ -0,0 +1,9 @@
+package com.store.movie
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MovieQueueApp : Application() {
+    // Add any application-level initialization logic here if needed
+}
